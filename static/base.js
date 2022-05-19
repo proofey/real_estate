839ca91c82c3ea8a5bcec13c5b1fe2062cbef041
Mincho Rusev <proofey89@gmail.com>
2022-05-19 22:42:14 +0300

    Add navbar

---
+++ static/base.js
@@ -0,0 +1,10 @@
+const navbarMenuToggle = document.getElementById('navbar-menu-toggle');
+navbarMenuToggle.addEventListener('click', (e)=> {
+    e.preventDefault()
+    const navbarMenuDropdown = document.getElementById('navbar-menu-dropdown');
+    if(navbarMenuDropdown.classList.contains('d-none')){
+        navbarMenuDropdown.classList.remove('d-none');
+    }else{
+        navbarMenuDropdown.classList.add('d-none');
+    }
+});
