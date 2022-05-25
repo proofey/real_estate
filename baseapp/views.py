2046010f7a57fcb7fe7a26d9c3a0bb5349aa86f4
Mincho Rusev <proofey89@gmail.com>
2022-05-25 22:44:28 +0300

    Finish pagination for main page

--- baseapp/views.py
+++ baseapp/views.py
@@ -5,7 +5,7 @@ from post.models import Post
 
 def home_page(request):
     posts = Post.objects.all()
-    paginator = Paginator(posts, 4)
+    paginator = Paginator(posts, 1)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     return render(request, 'baseapp/home_page.html', {
